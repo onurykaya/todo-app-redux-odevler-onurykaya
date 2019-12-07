1c2cd9623966de4afbd94ce5348245a0368db289
Onur Yerlikaya <onur.yerlikaya1@gmail.com>
2019-12-07 15:30:17 +0300

    Updates

---
+++ precache-manifest.629ccdf08ace1414910acb7bf46c1fdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3739c3c72d874d57ac8a1853e6f2f9da",
+    "url": "/todo-app-redux-odevler-onurykaya/index.html"
+  },
+  {
+    "revision": "271817b6383ee771f67b",
+    "url": "/todo-app-redux-odevler-onurykaya/static/css/main.3b3d84a9.chunk.css"
+  },
+  {
+    "revision": "75ca250ed521ea673670",
+    "url": "/todo-app-redux-odevler-onurykaya/static/js/2.17b57811.chunk.js"
+  },
+  {
+    "revision": "271817b6383ee771f67b",
+    "url": "/todo-app-redux-odevler-onurykaya/static/js/main.d588f6d5.chunk.js"
+  },
+  {
+    "revision": "86cc6e0d1cb87352915b",
+    "url": "/todo-app-redux-odevler-onurykaya/static/js/runtime-main.df6598e2.js"
+  }
+]);
